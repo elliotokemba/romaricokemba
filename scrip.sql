2c151cd4d4cffed1c80c3f58258b99b5c14fb300
romaric <ngromaricokemba@gmail.com>
2015-06-23 13:26:09 +0000

    Ajout

---
+++ scrip.sql
@@ -0,0 +1,4 @@
+create database jerry;
+create table compte (compte varchar(10) primary key,mpd varchar(40));
+insert into compte values ('romaric','passer');
+insert into compte values ('delice', 'passer');
